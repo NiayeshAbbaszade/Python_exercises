751e39ce0412a05ca361611c6d6315c7f1980177
NiyeshAbbszade <niayeshabbaszade.20@gmail.com>
2024-09-27 14:32:13 +0330

    commit

--- exercise_session_1/tamrin_2.py
+++ exercise_session_1/tamrin_2.py
@@ -1,15 +1,17 @@
 import random
-Number = random.randint(1,100)
 
-while True :
+number_to_guess = random.randint(1, 100)
+while True:
     try:
-        guss=int(input('enter_your_number'))
-        if Number >= guss :
-            print('the_number_is_larger')
-        elif Number<=guss:
-            print('the_number_is_smaller')
+        user_guess = int(input("yek adad bein 1 ta 100 hads bezanid: "))
+        if user_guess < 1 or user_guess > 100:
+            print("lotfan adadi bein 1 ta 100 vared konid.")
+        elif user_guess > number_to_guess:
+            print("adad mored nazar bozorgtar ast.")
+        elif user_guess < number_to_guess:
+            print("adad mored nazar koochaktar ast.")
         else:
-            print('very_nice_your_gussed_right')
-        break
-    except ValueError :
-        print('plaese_enter_correct_number')
+            print(f"tabrik! adad dorost ra hads zadid: {number_to_guess}")
+            break
+    except ValueError:
+        print("lotfan yek adad motabar vared konid.")
