6f9c75b179abde8689f031b59a445e472dd271ce
NiyeshAbbszade <niayeshabbaszade.20@gmail.com>
2024-10-12 16:25:52 +0330

    commit

--- exercise_session_1/tamrin_1.py
+++ exercise_session_1/tamrin_1.py
@@ -1,7 +1,7 @@
 while True :
     try:
         age=int(input("enter_your_age"))
-        if age <= 18 :
+        if age >= 18 :
             print('you_can_vote')
         else:
          print("you_can_not_vote")
